3db84f1c615ab101661bdf22c9db1fa6e57084fa
User Admin <user@example.com>
2023-12-16 19:52:47 +0300

    still works

--- hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -3,15 +3,19 @@ package ru.otus.hw.config;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Setter
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
+    @Value("${test.rightAnswersCountToPass:0}")
     private int rightAnswersCountToPass;
 
     // внедрить свойство из application.properties
+    @Value("${test.fileName:''}")
     private String testFileName;
 
     @Override
