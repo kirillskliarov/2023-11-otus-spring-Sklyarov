85fb9ab5be3d5bf2016f5e7f67c8ff2644939225
User Admin <user@example.com>
2024-01-15 21:01:01 +0300

    remove unused imports

--- hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,12 +2,7 @@ package ru.otus.hw.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.Locale;
 import java.util.Map;
